0e6f9166c61548d016e610c751c819bccbf4d3ba
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-12-28 15:36:59 -0300

    Branch initial commit

--- src/components/pages/Customers/Customers.jsx
+++ src/components/pages/Customers/Customers.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./Customers.module.css";
 import Header from "../../shared/Header/Header";
+import {useSelector} from "react-redux";
 
-function Customers() {
+function Customers({history}) {
+  const { data, loading, error } = useSelector((s) => s.Table_Selector);
   return (
     <div className={styles.container}>
       <Header title="Customers" />
