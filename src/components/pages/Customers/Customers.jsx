8e2b8657be52a694294971f0ce6eb4448e9bbae5
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-12-28 16:25:27 -0300

    Customer component initial functions added

--- src/components/pages/Customers/Customers.jsx
+++ src/components/pages/Customers/Customers.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./Customers.module.css";
 import Header from "../../shared/Header/Header";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
+import {fetchResourceList, filterData} from "../../../redux/actions/tableActions"
+import {ENDPOINT_CUSTOMERS as BASE_ENDPOINT} from "../../../constants";
 
 function Customers({history}) {
   const { data, loading, error } = useSelector((s) => s.Table_Selector);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchResourceList(BASE_ENDPOINT));
+  }, []);
+
+  function handleOnSearch(e) {
+    const text = e.target.value.toLowerCase();
+    dispatch(filterData(text, "business_name", "contact_name", "email"));
+  }
+
   return (
     <div className={styles.container}>
       <Header title="Customers" />
