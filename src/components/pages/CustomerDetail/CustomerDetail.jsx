12642b425fd1fae2bdc15c2c78fc187f412a64ec
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-12-30 10:18:51 -0300

    customerActions created

--- src/components/pages/CustomerDetail/CustomerDetail.jsx
+++ src/components/pages/CustomerDetail/CustomerDetail.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./cutomerDetail.module.css";
+import {ENDPOINT_CUSTOMERS as BASE_ENDPOINT} from "../../../constants";
+import {fetchResourceList} from "../../../redux/actions/customerActions";
 
 function CustomerDetail( {match, history} ) {
   const { loading, error, formData} = useSelector(
     (s) => s.Customers_Selector
   );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      fetchResourceList({
+        isNew: match.url.endsWith("new"),
+        url: {
+          GET: `${BASE_ENDPOINT}/${match.params.id}`,
+          PUT_POST: match.url.endsWith("new")
+          ? `${BASE_ENDPOINT}`
+          : `${BASE_ENDPOINT}/${match.params.id}`,
+        },
+      })
+    );
+  }, [history]);
 }
---
+++ src/redux/actions/customerActions.js
@@ -0,0 +1,113 @@
+import {
+  FETCH_RESOURCE_BEGIN,
+  FETCH_RESOURCE_SUCCESS,
+  FETCH_RESOURCE_FAILURE,
+  FORM_UPDATE_FIELD,
+  FORM_SUBMIT_BEGIN,
+  FORM_SUBMIT_SUCCESS,
+  FORM_SUBMIT_FAILURE,
+} from "../types/customers";
+
+let urlServer = "";
+let isNewResource = true;
+
+export function fetchResourceList({isNew, url}) {
+  return async (dispatch) => {
+    if(!url) return dispatch(fetchResourceFailure("Error: Invalid URL"));
+    urlServer = url;
+    isNewResource = isNew;
+    if (isNew) return;
+
+    dispatch (fetchResourceBegin());
+
+    try {
+      const response = await fetch(urlServer.GET);
+      const json = await response.json();
+      response.ok
+        ? dispatch(fetchResourceSuccess(json))
+        : dispatch(fetchResourceFailure(json.message));
+    } catch (error) {
+      dispatch(fetchResourceFailure(error.message));
+    }
+  };
+}
+
+function fetchResourceBegin() {
+  return {
+    type: FETCH_RESOURCE_BEGIN,
+  };
+}
+
+function fetchResourceSuccess(resources) {
+  return {
+    type: FETCH_RESOURCE_SUCCESS,
+    payload: { resources },
+  };
+}
+
+function fetchResourceFailure(error) {
+  return {
+    type: FETCH_RESOURCE_FAILURE,
+    payload: { error },
+  };
+}
+
+export function handleModifyFormData(event) {
+  return {
+    type: FORM_UPDATE_FIELD,
+    payload: { event },
+  };
+}
+
+export function handleSubmit(formData, history, match) {
+  return (dispatch) => {
+    dispatch(formSubmitBegin());
+
+    const formParseData = {...formData};
+
+    fetch(urlServer.PUT_POST, {
+      method: isNewResource ? "POST" : "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formParsedData),
+    }).then(
+      (r) => {
+        if (!r.ok)
+        return dispatch(
+          formSubmitFailure("Customer update failed!")
+        );
+
+        let newId = "";
+        if (isNewResource) {
+          r.json().then((_data) => {
+            newId = _data.id;
+            dispatch(formSubmitSuccess());
+            return history.push(`${match.path.replace(":id", newId)}`);
+          });
+        }
+        dispatch(formSubmitSuccess());
+      },
+      (error) => dispatch(formSubmitFailure(error.message))
+    );
+  };
+}
+
+function formSubmitBegin() {
+  return {
+    type: FORM_SUBMIT_BEGIN,
+  };
+}
+function formSubmitSuccess(newId) {
+  return {
+    type: FORM_SUBMIT_SUCCESS,
+    payload: { newId },
+  };
+}
+
+function formSubmitFailure(error) {
+  return {
+    type: FORM_SUBMIT_FAILURE,
+    payload: { error },
+  };
+}
