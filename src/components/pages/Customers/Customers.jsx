f0581ee28daf1540802e9c745af1c3b8a753e4ca
Alvaro A Pansa <alvaro.pansa@gmail.com>
2020-12-30 07:05:35 -0300

    Table added to customers component

--- src/components/pages/Customers/Customers.jsx
+++ src/components/pages/Customers/Customers.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Customers.module.css";
 import Header from "../../shared/Header/Header";
+import Table from "../../shared/Table/Table";
 import {useSelector, useDispatch} from "react-redux";
-import {fetchResourceList, filterData} from "../../../redux/actions/tableActions"
+import {fetchResourceList, filterData, deleteResource} from "../../../redux/actions/tableActions"
 import {ENDPOINT_CUSTOMERS as BASE_ENDPOINT} from "../../../constants";
 
 function Customers({history}) {
@@ -21,7 +22,58 @@ function Customers({history}) {
   return (
     <div className={styles.container}>
       <Header title="Customers" />
-      <div>Customers content</div>
+      {loading && <h3>Loading ...</h3>}
+      {error && <h3>{error}</h3>}
+      {data && (
+        <Table 
+          handleOnSearch={handleOnSearch}
+          data={data}
+          headers={[
+            {
+              order: 0,
+              displayName: "ID",
+              dataName: "id",
+            },
+            {
+              order: 1,
+              displayName: "Business Name",
+              dataName: "businessName",
+            },
+            {
+              order: 2,
+              displayName: "Contact Name",
+              dataName: "contactName",
+            },
+            {
+              order: 3,
+              displayName: "Email",
+              dataName: "email",
+            },
+            {
+              order: 4,
+              displayName: "Phone",
+              dataName: "phone",
+            },
+            {
+              order: 5,
+              displayName: "Fiscal Address",
+              dataName: "fiscalAddress",
+            },
+          ]}
+          actions={[
+            {
+              fn: (id) => history.push(`${history.location.pathname}/${id}`),
+              displayName: "✏",
+              hint: "Edit customer",
+            },
+            {
+              fn: (id) => dispatch(deleteResource(id)),
+              displayName: "❌",
+              hint: "Delete customer",
+            },
+          ]}
+        />
+      )}
     </div>
   );
 }
