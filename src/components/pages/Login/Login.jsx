6ac3e0f9ad2982c9818396225dd6b88862e26ee6
Alvaro A Pansa <alvaro.pansa@gmail.com>
2021-01-10 12:37:41 -0300

    Login component added

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,4 +1,10 @@
-
+import { Field, Form } from "react-final-form";
+import { bindActionCreators } from "redux";
+import Card from "../../shared/Card/Card";
+import { TextInput } from "../../shared/FormInputs/FormInputs";
+import { required } from "../../shared/FormInputs/formsValidations";
+import Header from "../../shared/Header/Header";
+import loginWithFirebase from "../../../redux/actions/authActions";
 
 const Login = ({
   login
@@ -6,4 +12,38 @@ const Login = ({
   const onSubmitLogin = (values) => {
     login(values);
   };
+
+  return (
+    <React.Fragment>
+      <Header title="Caldar APP" />
+      <Card title={"Login"}>
+        <Form 
+          onSubmit={onSubmitLogin}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+              <Field name="email" validate={required}>
+                {(props) => <TextInput {...props} label="Email" />}
+              </Field>
+              <Field name="password" validate={required}>
+                {(props) => <TextInput {...props} label="Email" />}
+              </Field>
+              <div className={styles.buttons}>
+                <button type="submit" disabled={submitting || pristine}>
+                  Login
+                </button>
+              </div>
+            </form>
+          )}
+        />
+      </Card>
+    </React.Fragment>
+  )
 };
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    login: loginWithFirebase
+  }, dispatch);
+};
+
+export default connect (null, mapDispatchToProps)(Login);
