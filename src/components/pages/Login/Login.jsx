6b2a4bcdd0273968f788cfd75a31bc5472b78fa8
Juan Wagner <juanswagner@gmail.com>
2021-02-01 19:38:36 -0300

    Center log in form

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -9,46 +9,55 @@ import Header from "../../shared/Header/Header";
 import { loginWithFirebase } from "../../../redux/actions/authActions";
 import { connect } from "react-redux";
 
-const Login = ({
-  login
-}) => {
+const Login = ({ login }) => {
   const onSubmitLogin = (values) => {
     login(values);
   };
 
   return (
-    <React.Fragment>
+    <>
       <Header title="Caldar APP" />
-      <Card title={"Login"}>
-        <div className={styles.container}>
-          <Form 
-            onSubmit={onSubmitLogin}
-            render={({ handleSubmit, form, submitting, pristine, values }) => (
-              <form onSubmit={handleSubmit}>
-                <Field name="email" validate={required}>
-                  {(props) => <TextInput {...props} label="Email" />}
-                </Field>
-                <Field name="password" validate={required}>
-                  {(props) => <TextInput {...props} label="Password" />}
-                </Field>
-                <div className={styles.buttons}>
-                  <button type="submit" disabled={submitting || pristine}>
-                    Login
-                  </button>
-                </div>
-              </form>
-            )}
-          />
-        </div>
-      </Card>
-    </React.Fragment>
-  )
+      <div className={styles.cardContainer}>
+        <Card title={"Login"}>
+          <div className={styles.container}>
+            <Form
+              onSubmit={onSubmitLogin}
+              render={({
+                handleSubmit,
+                form,
+                submitting,
+                pristine,
+                values,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <Field name="email" validate={required}>
+                    {(props) => <TextInput {...props} label="Email" />}
+                  </Field>
+                  <Field name="password" validate={required}>
+                    {(props) => <TextInput {...props} label="Password" />}
+                  </Field>
+                  <div className={styles.buttons}>
+                    <button type="submit" disabled={submitting || pristine}>
+                      Login
+                    </button>
+                  </div>
+                </form>
+              )}
+            />
+          </div>
+        </Card>
+      </div>
+    </>
+  );
 };
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    login: loginWithFirebase
-  }, dispatch);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      login: loginWithFirebase,
+    },
+    dispatch
+  );
 };
 
 export default connect(null, mapDispatchToProps)(Login);
